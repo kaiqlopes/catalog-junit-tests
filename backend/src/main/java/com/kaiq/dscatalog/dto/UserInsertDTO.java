706d69dba5a0b479e160236495b7816064f45baf
Kaique Lopes <kai-quelopes@hotmail.com>
2024-03-26 14:09:33 -0300

    Checking if the email already exist when updating user using custom constraint validator

--- backend/src/main/java/com/kaiq/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/kaiq/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.kaiq.dscatalog.dto;
 
+import com.kaiq.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private String password;
